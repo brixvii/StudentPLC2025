44c0d473c5516548da4bdea261735766200a181b
Bridget Osagiede <bosagiede76@gmail.com>
2025-02-03 06:23:47 +0000

    modifying lab2

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,10 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (MyFunc2 10) --correct version (myFunc2 10)
+    print (3rdFunc) --correct version (thirdFunc)
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+MyFunc2 x = x*2 --correct version myFunc2 x = x*2
+3rdFunc = "Hello, this is 3rd Func" --correct version thirdFunc = "Hello, this is 3rd Func"
+
--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,16 @@
 ask :: String -> IO ()
 ask prompt =
   do
-  putStrLn prompt
-  line <- getLine
-  if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    putStrLn prompt
+    line <- getLine
+    if line == "quit"  -- Check for "quit" to exit
+      then putStrLn "quitting..." >> return ()  -- Print "quitting..." and exit
+      else if line == ""  -- Handle empty input
+        then ask (prompt ++ "!") -- Call ask recursively if no input
+        else putStrLn ("you said: " ++ reverse line) >> ask prompt  -- Print reversed input and continue
 
 main :: IO ()
 main =
   do
-  let prompt = "please say something"
-  ask prompt
+    let prompt = "please say something"
+    ask prompt
